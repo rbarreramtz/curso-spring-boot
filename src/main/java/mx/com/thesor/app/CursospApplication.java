678ad40779d700c60fecb3c5ebe3833597095778
Juan Ramiro Barrera <jramiro05@hotmail.com.mx>
2024-07-03 22:26:00 -0600

    first commit

---
+++ src/main/java/mx/com/thesor/app/CursospApplication.java
@@ -0,0 +1,13 @@
+package mx.com.thesor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/thesor/app/CursospApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.thesor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
